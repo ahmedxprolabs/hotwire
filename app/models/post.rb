c0bdaf9d2242b37af42e266619ffe31c2b3478b5
ahmedxprolabs <ahmed.rasul@xprolabs.com>
2025-08-19 16:41:38 +0500

    changes added.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,4 @@ class Post < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     %w[title body created_at updated_at]
   end
-
-  def self.ransackable_associations(auth_object = nil)
-    []
-  end
 end
